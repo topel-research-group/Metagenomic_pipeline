8311009db6119b11a2b9a05de268dd27d1eef57d
mtop <mats@topel.se>
2018-06-08 10:38:58 +0200

    Corrected error in input file names

--- run_metagenomic_pipeline.py
+++ run_metagenomic_pipeline.py
@@ -12,8 +12,8 @@ MEM=232000000000
 # Megahit assembly
 for DIR in `find ./* -type d`; do
 
-	FILE1=${DIR}/*R1.*.fastq.gz
-	FILE2=${DIR}/*R2.*.fastq.gz
+	FILE1=${DIR}/*R1*.fastq.gz
+	FILE2=${DIR}/*R2*.fastq.gz
 
 	megahit -m $MEM -1 $FILE1 -2 $FILE2 -t $NSLOTS -o ${DIR}/Megahit --out-prefix ${DIR} 2> ${DIR}/${DIR}_assemble.log
 
